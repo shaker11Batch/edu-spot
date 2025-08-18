e678c2b8f1eab52707bb083a30107211b1d027d3
shaker11batch <saenterprise67811@gmail.com>
2025-08-18 18:45:47 +0600

    last c

--- src/pages/Contact/contact.jsx
+++ src/pages/Contact/contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <section className="max-w-7xl mx-auto px-4 py-12">
       <h2 className="text-3xl md:text-4xl font-bold text-center mb-8">
-        Contact
+        Contact Us
       </h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
